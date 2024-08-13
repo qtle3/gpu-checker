73205763fee04868b95b7e3f34ef7e9e9f73bea8
Quocviet Le <quocvietlet@gmail.com>
2024-08-12 22:36:39 -0500

    final pytorch gpu check

--- gpu_check.py
+++ gpu_check.py
@@ -2,14 +2,18 @@
 
 # import libraries
 import torch
-
+import tensorflow as tf
 
 # check if CUDA is available
-print("CUDA available:", torch.cuda.is_available())
-print("CUDA device count:", torch.cuda.device_count())
+print("PyTorch CUDA available:", torch.cuda.is_available())
+print("PyTorch CUDA device count:", torch.cuda.device_count())
 
+# print the GPU information
 if torch.cuda.is_available():
-    print("CUDA current device:", torch.cuda.current_device())
-    print("CUDA device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
+    print("PyTorch CUDA current device:", torch.cuda.current_device())
+    print(
+        "PyTorch CUDA device name:",
+        torch.cuda.get_device_name(torch.cuda.current_device()),
+    )
 else:
-    print("No CUDA device found")
+    print("No PyTorch CUDA device found")
